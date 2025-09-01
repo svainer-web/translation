bb4cbd0abcbd9e82fe8870111b1eb7052b4e9f3d
svainer <svainer@rafael.co.il>
2025-09-01 16:26:43 +0300

    update dictionaries by translated files

--- return Dictionary.py
+++ return Dictionary.py
@@ -9,20 +9,21 @@ def precess_xml(input_path):
     tree = ET.parse(input_path)
     root = tree.getroot()
 
-    print(f"Processing XML file: {input_path}")
+    # print(f"Processing XML file: {input_path}")
 
     heb_dictionary = {}
     eng_dictionary = {}
     
     create_dictionaries(input_path, heb_dictionary, eng_dictionary)
 
-    tagNames = {'.//Description', './/DisplayName', './/DisplayDescription', './/Notes' }
+    remove_missing_tags(root)   
+    
+    tagNames = {'.//Entry' }
     for tagName in tagNames:
-        print("Adding Hebrew descriptions...")
         return_strings_from_dictionary(root, tagName, heb_dictionary, eng_dictionary)
 
-    print("Setting titles...")
-    setTitle(root)
+    # print("Setting titles...")
+    # setTitle(root)
     # Write back to file
     xml_str = ET.tostring(root, encoding='utf-8')
 
@@ -36,53 +37,76 @@ def precess_xml(input_path):
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(pretty_xml)      
 
-def return_strings_from_dictionary(root, tagName, heb_dictionary, eng_dictionary):
-    for desc in root.findall(tagName):
+def remove_missing_tags(root):
+    for desc in root.findall('.//Value'):
         heb_elem = desc.find('Hebrew')
         if heb_elem is not None and heb_elem.text:
-            index = int(heb_elem.text.strip())  
+            if "NOT_TRANSLATED" in heb_elem.text:
+                desc.remove(heb_elem)
+        eng_elem = desc.find('English')
+        if eng_elem is not None and eng_elem.text:
+            if "NOT_TRANSLATED" in eng_elem.text:
+                desc.remove(eng_elem)
+
+def return_strings_from_dictionary(root, tagName, heb_dictionary, eng_dictionary):
+    for desc in root.findall(tagName):
+        index = int(desc.find('Key').text.strip())
+        value_elem = desc.find('Value');
+
+        heb_elem = value_elem.find('Hebrew')
+        if heb_elem is None:
+            heb_elem = ET.SubElement(value_elem, "Hebrew")
             text = heb_dictionary.get(index)
             if text is None:
                 heb_elem.text = f"NOT_TRANSLATED Missing text for index {index}"
             else:
                 heb_elem.text = text
-                
-            eng_elem = ET.Element('English')
+               
+        eng_elem = value_elem.find('English')
+        if eng_elem is None:
+            eng_elem = ET.SubElement(value_elem, "English")
             text = eng_dictionary.get(index)
             if text is None:
                 eng_elem.text = f"NOT_TRANSLATED Missing text for index {index}"
             else:
-                eng_elem.text = text
-            desc.append(eng_elem)
+                eng_elem.text = text            
 
 def create_dictionaries(input_path, heb_dictionary, eng_dictionary):
     path = Path(input_path)
     name = path.name
-    directory = path.parent
+    directory = path.parent.parent
     
-    heb_file_name = directory / 'translation/hebrew' / name.replace('.xml', '_dictionary_heb.txt')
+    heb_file_name = directory / 'hebrew' / name.replace('.xml', '_heb.txt')
     if heb_file_name.exists() and heb_file_name.is_file():
         with open(heb_file_name, "r", encoding="utf-8") as f:
             lines = f.readlines()
         heb_dictionary.update(read_dictionary_file(lines))
+    # else:
+    #     print(f"Hebrew file not found: {heb_file_name}")
 
-    heb_file_name = directory / 'translation/english/translated' / name.replace('.xml', '_dictionary_eng.txt')
+    heb_file_name = directory / 'english/translated' / name.replace('.xml', '_heb.txt')
     if heb_file_name.exists() and heb_file_name.is_file():
         with open(heb_file_name, "r", encoding="utf-8") as f:
             lines = f.readlines()
         heb_dictionary.update(read_dictionary_file(lines))
+    # else:
+    #     print(f"Hebrew file not found: {heb_file_name}")    
     
-    eng_file_name = directory / 'translation/english' / name.replace('.xml', '_dictionary_eng.txt')    
+    eng_file_name = directory / 'english' / name.replace('.xml', '_eng.txt')    
     if eng_file_name.exists() and eng_file_name.is_file():
         with open(eng_file_name, "r", encoding="utf-8") as f:
             lines = f.readlines()
         eng_dictionary.update(read_dictionary_file(lines))
+    # else:
+    #     print(f"English file not found: {eng_file_name}")   
     
-    eng_file_name = directory / 'translation/hebrew/translated' / name.replace('.xml', '_dictionary_eng.txt')
+    eng_file_name = directory / 'hebrew/translated' / name.replace('.xml', '_heb.txt')
     if eng_file_name.exists() and eng_file_name.is_file():
         with open(eng_file_name, "r", encoding="utf-8") as f:
             lines = f.readlines()
-        eng_dictionary.update(read_dictionary_file(lines)) 
+        eng_dictionary.update(read_dictionary_file(lines))
+    # else:
+    #     print(f"English file not found: {eng_file_name}")
 
 def more_hebrew_than_english(text: str) -> bool:
     hebrew_letters = re.findall(r'[\u0590-\u05FF]', text)
@@ -146,9 +170,10 @@ def flatten_xml_file(input_path):
     return flat_xml_as_string
 
 if __name__ == "__main__":
-    current_dir = os.getcwd()
-    files = os.listdir(current_dir)
+    current_dir = Path(__file__).parent
+    needed_dir = current_dir / 'DictionaryXmls'
+    files = os.listdir(needed_dir)
     for file in files:
         if file.endswith('.xml'):
-            input_path = os.path.join(current_dir, file)
+            input_path = os.path.join(needed_dir, file)
             precess_xml(input_path)
